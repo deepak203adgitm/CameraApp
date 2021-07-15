4c9e5f984886b24ca0e295ab463215e0b3f300c8
deepak203adgitm <73418732+deepak203adgitm@users.noreply.github.com>
2021-07-15 08:35:43 -0700

    media stream

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-
 let video = document.querySelector("video");
+let recordVideo = document.querySelector(".Record-video");
+let captureVideo = document.querySelector(".Capture-video");
 let constraints = {video :true};
-// (async function(){
-//  // 88
-//   var mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+let recordData ;
+var mediaRecorder;
 
-//   console.log(mediaStream);
-//   video.srcObject= mediaStream;
+let state = false;
 
-// })();
 
-var promise =  navigator.mediaDevices.getUserMedia(constraints);
+(async function(){
+ // 88
+  var mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
 
-promise.then(function(mediastream){
-    video.srcObject= mediastream;
-});
+  //console.log(mediaStream);
+  video.srcObject= mediaStream;
+  mediaRecorder = new MediaRecorder(mediaStream);
+    video.srcObject= mediaStream;
+    console.log(mediaRecorder);
 
+    mediaRecorder.onstop =function(e)
+    {
+
+    }
+
+    mediaRecorder.onstart =function(e)
+    {
+        
+    }
+
+    mediaRecorder.ondataavailable =function(e)
+    {
+        recordData  = e.data;
+        console.log(recordData);  
+    }
+
+
+recordVideo.addEventListener("click",function(){
+
+    if(state) mediaRecorder.stop();
+    else if(!state) mediaRecorder.start(); 
+
+   
+    state =!state;
+})
+
+
+})();
