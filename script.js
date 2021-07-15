3fcc55a73b90864979fef02c351e9c5a46acb12d
deepak203adgitm <73418732+deepak203adgitm@users.noreply.github.com>
2021-07-15 09:02:04 -0700

    some changes

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
 let video = document.querySelector("video");
 let recordVideo = document.querySelector(".Record-video");
 let captureVideo = document.querySelector(".Capture-video");
-let constraints = {video :true};
-let recordData ;
+let constraints = { video: true };
+let recordData;
 var mediaRecorder;
 
 let state = false;
 
 
-(async function(){
- // 88
-  var mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+(async function () {
+    // 88
+    var mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
 
-  //console.log(mediaStream);
-  video.srcObject= mediaStream;
-  mediaRecorder = new MediaRecorder(mediaStream);
-    video.srcObject= mediaStream;
+    //console.log(mediaStream);
+    video.srcObject = mediaStream;
+    mediaRecorder = new MediaRecorder(mediaStream);
+    video.srcObject = mediaStream;
     console.log(mediaRecorder);
 
-    mediaRecorder.onstop =function(e)
-    {
+    mediaRecorder.onstop = function (e) {
 
     }
 
-    mediaRecorder.onstart =function(e)
-    {
-        
+    mediaRecorder.onstart = function (e) {
+
     }
 
-    mediaRecorder.ondataavailable =function(e)
-    {
-        recordData  = e.data;
-        console.log(recordData);  
+    mediaRecorder.ondataavailable = function (e) {
+        recordData = e.data;
+        console.log(recordData);
+
+        savevideo();
     }
 
 
-recordVideo.addEventListener("click",function(){
+    recordVideo.addEventListener("click", function () {
 
-    if(state) mediaRecorder.stop();
-    else if(!state) mediaRecorder.start(); 
+        if (state) 
+        mediaRecorder.stop(), recordVideo.innerHTML ="Record";
+        else if (!state) mediaRecorder.start(), recordVideo.innerHTML ="Recording";
 
-   
-    state =!state;
-})
+
+        state = !state;
+    })
 
 
 })();
+
+
+function savevideo()
+{
+console.log("saving video ");
+
+let url =URL.createObjectURL(recordData);
+console.log(url);
+
+let aTag = document.createElement("a");
+aTag.download= "video.mp4";
+aTag.href =url;
+aTag.click();
+aTag.remove();
+
+
+}
